cc6cda96b3d3def804cab27602f234fc635dddf5
oritshuva <ori.tshuva87@gmail.com>
2025-01-04 22:22:16 +0200

    new work

--- app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
+++ app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
@@ -14,17 +14,21 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    EditText Num1,Num2;
+    EditText Num1, Num2;
     TextView tvResult;
     Button btnPlus;
     Button btnMinus;
     Button btnMult;
     Button btnDiv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -43,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Please enter both numbers", Toast.LENGTH_SHORT).show();
             return;
         }
+
 
         // המרת קלט למספרים
         int num1 = Integer.parseInt(et1Text);
