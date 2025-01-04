f12b8b82ef1dd2a63a713efa7adcbc7bcb94d003
oritshuva <ori.tshuva87@gmail.com>
2025-01-04 19:27:24 +0200

    new work

--- app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
+++ app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.mysimplenew57calc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +14,12 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText Num1,Num2;
+    TextView tvResult;
+    Button btnPlus;
+    Button btnMinus;
+    Button btnMult;
+    Button btnDiv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
+
+    public void onBtnClicked(View view) {
+        EditText et1 = findViewById(R.id.Num1);
+        EditText et2 = findViewById(R.id.Num2);
+
+        String et1Text = Num1.getText().toString().trim();
+        String et2Text = Num2.getText().toString().trim();
+
+        // בדיקה אם הקלט ריק
+        if (et1Text.isEmpty() || et2Text.isEmpty()) {
+            Toast.makeText(this, "Please enter both numbers", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // המרת קלט למספרים
+        Integer num1 = Integer.valueOf(et1Text);
+        Integer num2 = Integer.valueOf(et2Text);
+        int result = 0 ;
+
+        if (view.getId() == R.id.btnPlus)
+            result = num1 + num2;
+        if (view.getId() == R.id.btnMinus)
+            result = num1 - num2;
+        if (view.getId() == R.id.btnMult)
+            result = num1 * num2;
+        if (view.getId() == R.id.btnDiv)
+            result = num1 / num2; {
+            if ((num2 == 0)||(num1==0)) {
+                Toast.makeText(this, "Cannot divide by zero", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+        // הצגת התוצאה ב-TextView
+        tvResult.setText(String.valueOf(result));
+    }
+
 }
