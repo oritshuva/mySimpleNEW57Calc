b1e9085ebf66b9b968d7998b11e14c04ea26e7ef
oritshuva <ori.tshuva87@gmail.com>
2025-01-05 12:24:41 +0200

    new work

--- app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
+++ app/src/main/java/com/example/mysimplenew57calc/MainActivity.java
@@ -26,15 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
+        Num1 = findViewById(R.id.Num1);
+        Num2 = findViewById(R.id.Num2);
+        tvResult = findViewById(R.id.tvResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMult = findViewById(R.id.btnMult);
+        btnDiv = findViewById(R.id.btnDiv);
     }
 
     public void onBtnClicked(View view) {
